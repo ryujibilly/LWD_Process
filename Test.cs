31f22ddada9d387361562bf63c9779e947d7868b
ryujibilly <ryujibilly@qq.com>
2016-10-25 21:40:35 +0800

    1025-2140

---
+++ Test.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace LWD_DataProcess
+{
+    public partial class Test : Form
+    {
+        String pattern = "";
+        String example = "";
+        public Test()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            pattern = textBox1.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(Regex.IsMatch(example, pattern))
+            {
+                label3.BackColor = Color.Lime;
+                label3.Text = "√";
+            }
+            else
+            {
+                label3.BackColor = Color.Red;
+                label3.Text = "×";
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            example = textBox2.Text;
+        }
+
+    }
+}
